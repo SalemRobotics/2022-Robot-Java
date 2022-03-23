37ab2642f78863c1a9980d888a4d4762c711f6eb
inky <sau57robotics@gmail.com>
2022-03-23 17:55:43 -0400

    Updated docs for subsystem and commands.

--- src/main/java/frc/robot/commands/DefaultDrive.java
+++ src/main/java/frc/robot/commands/DefaultDrive.java
@@ -4,12 +4,12 @@ import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import java.util.function.DoubleSupplier;
 
+/** Command to set arcade drive */
 public class DefaultDrive extends CommandBase {
   private final Drivetrain drive;
   private final DoubleSupplier forward;
   private final DoubleSupplier rotation;
 
-  // Command to set arcade drive
   public DefaultDrive(Drivetrain subsystem, DoubleSupplier fwd, DoubleSupplier rot) {
     drive = subsystem;
     forward = fwd;
