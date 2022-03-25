9aab369696bb709e8e342fc1903088ec9f03b6b8
inky <sau57robotics@gmail.com>
2022-03-25 11:39:49 -0400

    Began auto/PID config

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,10 +7,34 @@ public final class Constants {
         public static final int kLeftRearPort = 1;
         public static final int kRightFrontPort = 2;
         public static final int kRightRearPort = 3;
+
+        public final class DriveTrainPIDConstants {
+            public static final double turnTravelUnitsPerRotation = 3600;
+            public static final double pigeonUnitsPerRotation = 8192;
+            public static final double P = 2.0;
+            public static final double I = 0.0;
+            public static final double D = 4.0;
+            public static final double F = 0.0;
+            public static final double Iz = 200;
+            public static final double peak = 1.0;
+        }
     }
 
     public final class XBConstants {
         public static final int drivePort = 0;
         public static final int opPort = 1;
+    }
+
+    public final class SpeedConstants {
+        public static final double targetHeight = 8.7; //ft
+        public static final double shootHeight = 2.73; //ft
+        public static final double goalOffset = 2.0; //ft
+        public static final double robotOffsetX = 0.0; //TODO: distance from limelight to shooter
+        public static final double shootAngle = 78.93; //degrees
+        public static final double gravity = 32.2; //ft/s^2
+        public static final double shooterWheelCircumference = 2 * Math.PI * 2.5;
+        public static final double alignmentAcceptableError = 0.5; //degrees
+        public static final double cameraHeight = 34.5; //inches
+        public static final double cameraAngle = 32.0; //degrees
     }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,21 +1,39 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.FollowerType;
+import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.TalonFXSensorCollection;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.PigeonIMU;
 
 import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.DrivetrainConstants.DriveTrainPIDConstants;
 
 /** Drivetrain uses 4 TalonFX motors, using differntial drive or "tank" drive. */
 public class Drivetrain extends SubsystemBase {
 
   WPI_TalonFX leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor;
+  TalonFXConfiguration leftConfig, rightConfig;
+  TalonFXSensorCollection leftEncoder, rightEncoder;
+  PigeonIMU gyro;
   MotorControllerGroup leftMotors, rightMotors;
   public static DifferentialDrive difDrive;
+  DifferentialDriveOdometry odometry;
 
   public Drivetrain() {
+
+    //left motors
     leftFrontMotor = new WPI_TalonFX(DrivetrainConstants.kLeftFrontPort);
     leftRearMotor = new WPI_TalonFX(DrivetrainConstants.kLeftRearPort);
     leftMotors = new MotorControllerGroup(
@@ -23,6 +41,7 @@ public class Drivetrain extends SubsystemBase {
       leftRearMotor
     );
 
+    //right motors
     rightFrontMotor = new WPI_TalonFX(DrivetrainConstants.kRightFrontPort);
     rightRearMotor = new WPI_TalonFX(DrivetrainConstants.kRightRearPort);
     rightMotors = new MotorControllerGroup(
@@ -31,7 +50,38 @@ public class Drivetrain extends SubsystemBase {
     );
     rightMotors.setInverted(true);
 
+    // Create motor configs
+    leftConfig = new TalonFXConfiguration();
+    rightConfig = new TalonFXConfiguration();
+
+    // set remote sensor source
+    leftConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.GadgeteerPigeon_Yaw;
+    leftConfig.remoteFilter0.remoteSensorDeviceID = gyro.getDeviceID();
+
+    // set PID sensor
+    leftConfig.auxiliaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor0;
+    leftConfig.auxiliaryPID.selectedFeedbackCoefficient = 
+    DriveTrainPIDConstants.turnTravelUnitsPerRotation / DriveTrainPIDConstants.pigeonUnitsPerRotation;
+    
+    // PID config
+    leftConfig.slot1.kP = DriveTrainPIDConstants.P;
+    leftConfig.slot1.kI = DriveTrainPIDConstants.I;
+    leftConfig.slot1.kD = DriveTrainPIDConstants.D;
+    leftConfig.slot1.kF = DriveTrainPIDConstants.F;
+    leftConfig.slot1.integralZone = DriveTrainPIDConstants.Iz;
+    leftConfig.slot1.closedLoopPeakOutput = DriveTrainPIDConstants.peak;
+    leftConfig.slot1.allowableClosedloopError = 0;
+
     difDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+    leftEncoder = leftFrontMotor.getSensorCollection();
+    rightEncoder = rightFrontMotor.getSensorCollection();
+
+    gyro = new PigeonIMU(1);
+
+    odometry = new DifferentialDriveOdometry(getRotation2d());
+
+
   }
 
   /**
@@ -42,5 +92,23 @@ public class Drivetrain extends SubsystemBase {
   public void arcadeDrive(double fwd, double rot) {
     difDrive.arcadeDrive(-fwd, rot, true);
   }
-  
+
+  public Rotation2d getRotation2d() {
+    double[] ypr = {};
+    gyro.getYawPitchRoll(ypr);
+    return Rotation2d.fromDegrees(ypr[0] * -1);
+  }
+
+  /** Zeroes the gyro's heading */
+  public void zeroHeading() {
+    gyro.setFusedHeading(0.0);
+  }  
+
+  public void alignToTarget(double errorAngle) {
+    zeroHeading();
+    leftFrontMotor.selectProfileSlot(1, 1);
+    leftFrontMotor.set(TalonFXControlMode.PercentOutput, 0.0, DemandType.AuxPID, errorAngle);
+    rightFrontMotor.follow(leftFrontMotor, FollowerType.AuxOutput1);
+    rightFrontMotor.setInverted(TalonFXInvertType.OpposeMaster);
+  }
 }
