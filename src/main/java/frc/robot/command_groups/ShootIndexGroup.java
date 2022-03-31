a7ccfe7e8efcde8a45d93b1450d0efde04988f2c
frc6324-team-laptop <frc6324-team-laptop>
2022-03-31 16:25:22 -0700

    wait command testing in shooter group

--- src/main/java/frc/robot/command_groups/ShootIndexGroup.java
+++ src/main/java/frc/robot/command_groups/ShootIndexGroup.java
@@ -1,16 +1,19 @@
 package frc.robot.command_groups;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.IndexIn;
 import frc.robot.commands.Shoot;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Shooter;
 
 
-public class ShootIndexGroup extends ParallelCommandGroup {
+public class ShootIndexGroup extends SequentialCommandGroup {
     public ShootIndexGroup(Shooter shooter, Indexer indexer) {
         addCommands(
             new Shoot(shooter),
+
+            new WaitCommand(1.5),
 
             new IndexIn(indexer)
         );
