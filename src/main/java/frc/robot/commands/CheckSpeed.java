e192dc622053387a22d52013fbb1569ecd413a3a
frc6324-team-laptop <frc6324-team-laptop>
2022-04-01 05:42:32 -0700

    New checkspeed command for batter shooting

---
+++ src/main/java/frc/robot/commands/CheckSpeed.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class CheckSpeed extends CommandBase implements BooleanSupplier {
+    private final Shooter shooter;
+    public CheckSpeed(Shooter subsystem) {
+        shooter = subsystem;
+        addRequirements(shooter);
+    }
+
+    @Override
+    public void execute() {}
+
+    @Override
+    public boolean getAsBoolean() {
+        return shooter.dumbCheckSpeed(14000);
+    }
+}
